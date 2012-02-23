f728ca80f61f52922842bf5f2f6f2384788afc12
Zachary Bush <zmbush@berkeley.edu>
2012-02-22 20:17:39 -0800

    Dynamic page is working.

--- js/term.js
+++ js/term.js
@@ -1,2 +1,73 @@
-function term(){
+---
+---
+var p = "guest@zmbush.com $ "
+var text = '<br /><br /><br />' + p
+var command = ""
+var cursor = "_"
+var accepting_input = true;
+$(function(){
+  $(document).keypress(function(e){
+    if(accepting_input){
+      switch(e.which){
+        case 13:
+          text += command;
+          text += '<br />';
+          accepting_input = false;
+          processCommand(command);
+          break;
+        default:
+          command += String.fromCharCode(e.which);
+          break;
+      }
+
+      $('#term').html(text + command + cursor);
+    }
+  });
+  $(document).keydown(function(e){
+    if(accepting_input){
+      if(e.which == 8){
+        command = command.substring(0, command.length - 1)
+        $('#term').html(text + command + cursor);
+      }
+    }
+  });
+  $('#term').html(text);
+});
+
+function processCommand(cin){
+  switch(cin){
+    case 'linkedin':
+      window.location = "http://www.linkedin.com/pub/zachary-bush/1a/a78/671";
+      break;
+    case 'github':
+      window.location = "https://github.com/zipcodeman";
+      break;
+    case 'static':
+      window.location = '/static/';
+      break;
+    case 'exit':
+      window.location = 'http://www.google.com/';
+      break;
+    default:
+      $.ajax({
+        url: 'output/' + cin,
+        dataType: "html",
+        error: function(){
+          displayOutput(cin + ": command not found");
+        },
+        success: function(output){
+          displayOutput(output);
+        }
+      });
+  }
+}
+
+function displayOutput(output){
+  text += output;
+  text += "<br />"
+  text += p;
+  command = '';
+  accepting_input = true;
+  $('#term').html(text);
+  $("html, body").animate({ scrollTop: $(document).height() }, "slow");
 }
