aab70c91497206d573bc0e3b7ecb75abd1748666
Zachary Bush <zmbush@berkeley.edu>
2013-04-02 11:37:09 -0700

    Update graphviz plugin

--- _plugins/graphviz.rb
+++ _plugins/graphviz.rb
@@ -5,11 +5,24 @@ module Jekyll
       @@visNum ||= 0
       @@visNum += 1
       @markup = markup
+      params = @markup.split(" ")
+      @name = nil
+      if params.length > 0
+        @name = params[0]
+      end
+      @algorithm = "dot"
+      if params.length > 1
+        @algorithm = params[1]
+      end
     end
 
     def render(context)
-      IO.popen("echo '#{@nodelist[0]}' | dot -Tpng -o'_site/images/gv-#{@@visNum}.png'") 
-      "<img src='/images/gv-#{@@visNum}.png' />"
+      fname = "gv-#{@@visNum}.png"
+      if ! @name.nil?
+        fname = "#{@name}.png"
+      end
+      IO.popen("echo '#{@nodelist[0]}' | #{@algorithm} -Tpng -o'_site/images/#{fname}'") 
+      "<img src='/images/#{fname}' />"
     end
   end
 end
